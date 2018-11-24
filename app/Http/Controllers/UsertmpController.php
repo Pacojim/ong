2c9bb41ecdaacda6314b20b0763e762954e8a59c
Paco <paco@vzenix.es>
2018-11-24 16:39:11 +0100

    vistas

---
+++ app/Http/Controllers/UsertmpController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use DB;
+
+class UserController extends BaseController {
+
+    public function users() {
+        $users = DB::select('select * from users where user_is_active = ?', [1]);
+
+        return view("ong.users", ['users' => $users]);
+    }
+
+    public function user($user) {
+        $users = DB::select('select * from users where id = ?', [$user]);
+
+        return view("ong.user", ['user' => $users[0]]);
+    }
+    
+
+}
